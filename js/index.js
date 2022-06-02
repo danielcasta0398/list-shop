587f4768903001fc1a36d7e50fd653661776d3ff
juancastano0398 <juancastano0398@gmail.com>
2022-06-02 09:32:11 +0200

    autofocus

--- js/index.js
+++ js/index.js
@@ -4,10 +4,12 @@ const changeState = (event) => {
     //El evento se encarga de capturar el click
     //event.stopPropagation lo que hace es detener el evento del hijo
     //event.stopPropagation
+   
     let popUp= document.getElementById('addPopUp');
     //El if es para hacer que el boton de + abra el popUp y el else es para que se cierre
     if(popUp.style.display == ''){
         popUp.style.display= "flex";
+        document.getElementById('product').focus()
     }
     else{
         popUp.style.display= "";
@@ -18,6 +20,7 @@ const changeState = (event) => {
 //La segunda funcion hace funcionar el boton de agregar producto dentro del popUp
 const addProduct = () => {
     let product = document.getElementById('product').value;
+    
     console.log(product);
     changeState();
     document.getElementById('product').value = "";
